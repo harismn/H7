698543f0ae2a302fcd9433520053067c6f737235
harismn <harismannug@gmail.com>
2018-04-04 13:15:01 +0700

    layout for vocab

--- src/views/tinder/card1.js
+++ src/views/tinder/card1.js
@@ -5,13 +5,25 @@ import {
   Text,
   View,
   TouchableOpacity,
+  TextInput,
 } from 'react-native';
 
 export default class Card1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
     render(){
         return(
-          <View style={styles.slide1}>
-            <Text style={styles.text}>This is Text</Text>
+          <View style={styles.container}>
+            <Text style={styles.text}>Vocab 1</Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+            <Text style={styles.secondText}>Hasil Translate</Text>
             <TouchableOpacity style={styles.button}
             onPress={() => console.log('aku keluar nih')}>
             <Text> Berikutnya </Text>
@@ -23,21 +35,32 @@ export default class Card1 extends Component {
 
 
 const styles = StyleSheet.create({
-    slide1: {
+    container: {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
+
     },
-    text: {
+    text: { 
       color: '#000',
       fontSize: 30,
       fontWeight: 'bold',
-      margin: 50,
+      margin: 20,
+      fontFamily: 'text-align',
+
     },
     button: {
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
-    padding: 10
-
+    padding: 10,
+    borderRadius: 15,
     },
+    textInput: {
+      height: 40, 
+      margin: 50,
+    },
+    secondText: {
+      height: 40, 
+      margin: 50,
+    }
   })
